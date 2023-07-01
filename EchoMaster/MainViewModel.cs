7198fec6036c5d9a06a0b17251d0c17986977e54
Hasan-Ali-Jebur <hassn.ali1212@yahoo.com>
2023-07-01 22:17:20 +0300

    انشاء المشروع بأستخدام الدف أكربيس وتفعيل خاصية mvvm

---
+++ EchoMaster/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EchoMaster
+{
+    [POCOViewModel()]
+    public class MainViewModel
+    {
+
+    }
+}
